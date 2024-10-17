ae53746366d95df4e7dcf7418aa8c3a6c5a0f511
Harshkhosla <harshkhosla9945@gmail.com>
2024-10-18 00:13:38 +0530

    adding the controller to the

---
+++ controllers/expenseController.js
@@ -0,0 +1,32 @@
+const Expense = require('../models/expense');
+const User = require('../models/user');
+
+exports.addExpense = async (req, res) => {
+  const { title, totalAmount, participants, createdBy } = req.body;
+  try {
+    const expense = new Expense({ title, totalAmount, participants, createdBy });
+    await expense.save();
+    res.status(201).json(expense);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getUserExpenses = async (req, res) => {
+  try {
+    const expenses = await Expense.find({ 'participants.userId': req.params.userId });
+    res.json(expenses);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getOverallExpenses = async (req, res) => {
+  try {
+    const expenses = await Expense.find();
+    res.json(expenses);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const User = require('../models/user');
+const Joi = require('joi');
+
+
+exports.createUser = async (req, res) => {
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    mobile: Joi.string().required(),
+  });
+
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).json({ error: error.details[0].message });
+
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getUserDetails = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return res.status(404).json({ error: 'User not found' });
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
